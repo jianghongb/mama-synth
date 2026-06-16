789ee07069956b0d441b0b7614329b028f6ea3d1
Hong Jiang <hong.jiang@ericsson.com>
2026-06-16 16:19:21 +0200

    fix: correct SBATCH account and conda env in preprocess script

--- src/preprocessing/run_mask_preprocess.sh
+++ src/preprocessing/run_mask_preprocess.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
-#SBATCH -A berzbiomedicalimagingkth
+#SBATCH -A berzelius-2025-422
+#SBATCH -p berzelius
 #SBATCH --gpus=1
 #SBATCH -t 4:00:00
 #SBATCH -J mask_preprocess
-#SBATCH --output=slurm-%j.out
+#SBATCH -o /proj/berzbiomedicalimagingkth/users/x_honji/mask_preprocess_%j.log
+#SBATCH -e /proj/berzbiomedicalimagingkth/users/x_honji/mask_preprocess_%j.err
 
-module load Anaconda/2023.09-0
-source activate nnunet  # or your conda env with nnunetv2
+PROJ=/proj/berzbiomedicalimagingkth/users/x_honji
 
-BASE="/proj/berzbiomedicalimagingkth/users/x_honji"
+export CONDA_PKGS_DIRS=$PROJ/.conda/pkgs
+export PIP_CACHE_DIR=$PROJ/.pip_cache
+export XDG_CACHE_HOME=$PROJ/.cache
+
+source activate /proj/berzbiomedicalimagingkth/users/x_honji/.conda/envs/pix2pix || \
+    source /proj/berzbiomedicalimagingkth/users/x_honji/miniconda3/bin/activate pix2pix
 
 export nnUNet_raw="/tmp/nnUNet_raw"
 export nnUNet_preprocessed="/tmp/nnUNet_preprocessed"
 export nnUNet_results="/tmp/nnUNet_results"
 mkdir -p $nnUNet_raw $nnUNet_preprocessed $nnUNet_results
 
-# Sync mask_and_preprocess.py from mama-synth repo (or copy it over)
-SCRIPT="${BASE}/mama-synth/src/preprocessing/mask_and_preprocess.py"
+SCRIPT="${PROJ}/mama-synth/src/preprocessing/mask_and_preprocess.py"
 
 python $SCRIPT \
-    --image_dir ${BASE}/images \
-    --seg_dir ${BASE}/segmentations/automatic \
-    --output_dir ${BASE}/data_split_v5/train \
-    --global_stats ${BASE}/mama-synth/src/preprocessing/training_pre_stats.json \
-    --breast_model_dir ${BASE}/exp4x_for_maia/Dataset932/nnUNetTrainer__nnUNetPlans__3d_fullres \
+    --image_dir ${PROJ}/images \
+    --seg_dir ${PROJ}/segmentations/automatic \
+    --output_dir ${PROJ}/data_split_v5/train \
+    --global_stats ${PROJ}/mama-synth/src/preprocessing/training_pre_stats.json \
+    --breast_model_dir ${PROJ}/exp4x_for_maia/Dataset932/nnUNetTrainer__nnUNetPlans__3d_fullres \
     --skip_ambiguous_shapes \
-    --exclude_list ${BASE}/mama-synth/src/preprocessing/motion_cases.txt \
+    --exclude_list ${PROJ}/mama-synth/src/preprocessing/motion_cases.txt \
     --device cuda
 
 echo "Done!"
