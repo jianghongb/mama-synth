35b603fd9af5f695d2f9c7c4084a703ba2b42978
Hong Jiang <hong.jiang@ericsson.com>
2026-07-02 16:17:07 +0200

    v26b: add breast_mask_dir

--- src/submission/submission-synthesis/models/scripts/berzelius_train_v26b_msv2.sh
+++ src/submission/submission-synthesis/models/scripts/berzelius_train_v26b_msv2.sh
@@ -68,6 +68,7 @@ python train.py \
   --lambda_vgg 10 \
   --num_D 2 \
   --n_layers_D 3 \
+  --breast_mask_dir $PROJ/data_multislice_v2/train/mha/breast_mask \
   --save_epoch_freq 5 \
   --print_freq 100 \
   --gpu_ids 0
